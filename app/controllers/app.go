581ab81e4b5abe5b6d43dbcd07180350a6024ee9
codymalick <cody.malick@gmail.com>
2017-04-06 17:16:43 -0700

    Update

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -12,6 +12,18 @@ func (c App) Index() revel.Result {
 	return c.Render()
 }
 
-func (c App) Comment() revel.Result {
-	return c.Render()
+func (c App) Comment(message string) revel.Result {
+	return c.Render(message)
+}
+
+func (c App) SendComment(message string) revel.Result {
+    c.Validation.Required(message).Message("Your message is required!")
+
+    if c.Validation.HasErrors() {
+        c.Validation.Keep()
+        c.FlashParams()
+        return c.Redirect(App.Comment)
+    }
+
+    return c.Render(message)
 }
